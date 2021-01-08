081f19b6336558e529f34af3e5798552e03300d8
Wendy C <chriscowendy@gmail.com>
2021-01-07 17:53:48 -0800

    There is something wrong
    In making the delete todo items section, it doesn't seem to run right. I need to look through the code and see what is missing.

--- js/script.js
+++ js/script.js
@@ -17,6 +17,16 @@ function toggleDone(key) {
     renderTodo(todoItems[index]);
 }
 
+function deleteTodo(key) {
+    const index = todoItems.findIndex(item => item.id === Number(key));
+    const todo = {
+        delete: true,
+        ...todoItems[index],
+    };
+    todoItems = todoItems.filter(item => item.id !== Number(key));
+    renderTodo(todo);
+}
+
 const form = document.querySelector('.js-form');
 form.addEventListener('submit', event => {
     event.preventDefault();
@@ -32,9 +42,14 @@ form.addEventListener('submit', event => {
 
 function renderTodo(todo) {
     const list = document.querySelector('.js-todo-list');
+    const item = document.querySelector(`[data-key='${todo.id}]`);
+
+    if (todo.deleted) {
+        item.remove();
+        return;
+    }
 
     const isChecked = todo.checked ? 'done': '';
-
     const node = document.createElement("li");
 
     node.setAttribute('class', `todo-item ${isChecked}`);
@@ -48,8 +63,11 @@ function renderTodo(todo) {
         <svg><use href="#delete-icon"></use></svg>
         </button>    
     `;
-    
-    list.append(node);
+    if (item) {
+        list.replaceChild(node, item);
+    } else {
+        list.append(node);
+    }
 }
 
 const list = document.querySelector('.js-todo-list');
@@ -58,6 +76,11 @@ list.addEventListener('click', event => {
     if (event.target.classList.contains('js-tick')) {
         const itemKey = event.target.parentElement.dataset.key;
         toggleDone(itemKey);
+    }
+
+    if (event.target.classList.contains('js-delete-todo')) {
+        const itemKey = event.target.parentElement.dataset.key;
+        deleteTodo(itemKey);
     }
 });
 
